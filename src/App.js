2975ded0a8776e2314ca641f6d464f4d6b6a2f9c
Eugene-prog <ieugene.prog@gmail.com>
2023-05-27 19:32:11 +0700

    feat: add reducer, state, action

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
+import { useDispatch, useSelector } from 'react-redux';
 import './App.css';
 
 function App() {
+  const dispatch  = useDispatch();
+  const cash = useSelector(state => state.cash);
+
   return (
     <div className="App">
-      
+        <div style={{display: "flex", flexDirection: 'column'}}>
+          <h1>{cash}</h1>
+          <div style={{display: "flex"}}>
+            <button>Пополнить счет</button>
+            <button>Снять со счета</button>
+          </div>
+        </div>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,7 +1,35 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
 import './index.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
 import App from './App';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+const action = {
+  type: '',
+  payload: '' 
+}
+
+const defaultState = {
+  cash: 5,
+}
+
+const reducer = (state = defaultState, action) => {
+  switch(action.type){
+    case "ADD_CASH": 
+      return {...state, cash: state.cash + action.payload}
+    case "GET_CASH":
+       return {...state, cash: state.cash - action.payload}
+    default: 
+      return state;
+  }
+}
+
+const store = createStore(reducer);
+
+
+ReactDOM.render(
+<Provider store={store}>
+  <App />
+</Provider>, document.getElementById('root'));
+
